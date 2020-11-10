8f4597f67f4053fb1a8ad9a09686e96c70ef125c
Roger Esteve <rogeresteve30@gmail.com>
2020-11-10 17:59:29 +0100

    Layout changes and activity info

--- app/src/main/java/com/example/periodictable/Element.java
+++ app/src/main/java/com/example/periodictable/Element.java
@@ -1,35 +1,50 @@
 package com.example.periodictable;
 
 public class Element {
-
-    private String symbol, atomicMass, name, state;
+    private String symbol;
+    private String type;
     private int atomicNumber;
+    private String name;
+    private String atomicMass;
+    private String elecConfig;
+    private String defaultState;
 
-    public Element(String symbol, int atomicNumber, String name, String atomicMass, String state) {
+    public Element(String symbol, int atomicNumber, String type, String name, String atomicMass, String elecConfig, String defaultState) {
         this.symbol = symbol;
+        this.type = type;
         this.atomicNumber = atomicNumber;
         this.name = name;
         this.atomicMass = atomicMass;
-        this.state = state;
+        this.elecConfig = elecConfig;
+        this.defaultState = defaultState;
     }
 
+    // GETTERS
     public String getSymbol() {
         return symbol;
     }
 
-    public String getAtomicMass() {
-        return atomicMass;
+    public String getType() {
+        return type;
+    }
+
+    public int getAtomicNumber() {
+        return atomicNumber;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getState() {
-        return state;
+    public String getAtomicMass() {
+        return atomicMass;
     }
 
-    public int getAtomicNumber() {
-        return atomicNumber;
+    public String getElecConfig() {
+        return elecConfig;
+    }
+
+    public String getDefaultState() {
+        return defaultState;
     }
 }
---
+++ app/src/main/java/com/example/periodictable/ElementInfoActivity.java
@@ -0,0 +1,32 @@
+package com.example.periodictable;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ElementInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_element_info);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "WIP", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
